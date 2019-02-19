d8137d5dd54dc428fc705f54c0c64d2245fee6ce
Cesar Correchel <cesarcorrechel@gmail.com>
2019-02-18 23:21:53 -0600

    moved blocks to src folder

--- gh-blocks.php
+++ gh-blocks.php
@@ -1,14 +1,39 @@
 <?php
 /**
  * Plugin Name: Gutenberg's Hotel Blocks
- * Plugin URI: http://github.com/cesarcor
+ * Plugin URI: https://github.com/cesarcor/gutenbergs-hotel-blocks
  * Description: Gutenberg's Hotel Block, useful blocks for hotel websites
  * Author: Cesar Correchel
  * Author URI: http://github.com/cesarcor
  * Version: 0.1
  *
- * @package gh-blocks
+ * @package gutenberg-hotel-blocks
  */
+
+
+ if ( ! defined( 'ABSPATH' ) ) {
+ 	exit;
+ }
+
+define('GH_PLUGIN_VERSION', '0.0.1');
+
+define( 'GH_BLOCKS_PATH', plugin_dir_path( __FILE__ ) );
+define( 'GH_BLOCKS_URL', plugin_dir_url( __FILE__ ) );
+
+function gh_enqueue_blocks(){
+
+    wp_register_script(
+        'hero-block',
+         GH_BLOCKS_URL . 'dist/blocks.build.js',
+         array( 'wp-blocks', 'wp-element', 'wp-editor' )
+       );
+
+   register_block_type( 'gutenbergs-hotel-blocks/hero-block', array(
+      'editor_script' => 'hero-block',
+   ) );
+}
+add_action( 'init', 'gh_enqueue_blocks' );
+
 
 add_filter('block_categories', function ($categories, $post) {
     return array_merge(
