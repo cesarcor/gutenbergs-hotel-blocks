86632816f29504ed6650a11fe4ad947c74039335
Cesar Correchel <cesarcorrechel@gmail.com>
2019-09-01 12:40:13 -0600

    add new project config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,81 @@
-module.exports = {
-	entry: './src/blocks/hero-block/index.js',
-	output: {
-		path: __dirname,
-		filename: 'dist/blocks.build.js',
-	},
-	module: {
-		loaders: [
-			{
-				test: /.js$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-			},
+const autoprefixer = require('autoprefixer');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+
+module.exports =  (env, argv) => {
+	function isDevelopment() {
+		return argv.mode === 'development';
+	}
+	var config = {
+		entry: './src/editor.js',
+		output: {
+			filename: 'editor.js'
+		},
+		optimization: {
+			minimizer: [
+				new TerserPlugin({
+					sourceMap: true
+				}),
+				new OptimizeCSSAssetsPlugin(
+				{
+					cssProcessorOptions: {
+						map: {
+							inline: false,
+							annotation: true
+						}
+					}
+				})
+			]
+		},
+		plugins: [
+			new CleanPlugin(),
+			new MiniCSSExtractPlugin({
+				filename: "editor.css"
+			})
 		],
-	},
-};
+		devtool: isDevelopment() ? 'cheap-module-source-map' : 'source-map',
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: [
+								'@babel/preset-env',
+								[
+									'@babel/preset-react',
+									{
+										"pragma": "wp.element.createElement",
+										"pragmaFrag": "wp.element.Fragment",
+										"development": isDevelopment()
+									}
+								]
+							]
+						}
+					}
+				},
+				{
+					test: /\.(sa|sc|c)ss$/,
+					use: [
+						MiniCSSExtractPlugin.loader,
+						'css-loader',
+						{
+							loader: 'postcss-loader',
+							options: {
+								plugins: [
+									autoprefixer()
+								]	
+							}
+						},
+						'sass-loader'
+					]
+				}
+			]
+		}
+	};
+	return config;
+}
