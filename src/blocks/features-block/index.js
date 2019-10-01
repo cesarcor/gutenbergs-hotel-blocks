28b3de6a2acfb5f6d96bb9ef453c146754b14430
Cesar Correchel <cesarcorrechel@gmail.com>
2019-10-01 12:36:42 -0600

    change name to -feature-

--- src/blocks/features-block/index.js
+++ src/blocks/features-block/index.js
@@ -1,50 +1,53 @@
-import './styles.editor.scss';
-import { registerBlockType } from '@wordpress/blocks';
+import "./styles.editor.scss";
+import "./parent";
+import { registerBlockType } from "@wordpress/blocks";
 import { RichText } from "@wordpress/editor";
-import { __ } from '@wordpress/i18n';
-import edit from './edit';
+import { __ } from "@wordpress/i18n";
+import edit from "./edit";
 
 const attributes = {
-  title:{
-    type: 'string',
-    source: 'html',
-    selector: 'h3'
+  title: {
+    type: "string",
+    source: "html",
+    selector: "h3"
   },
-  content:{
-    type: 'string',
-    source: 'html',
-    selector: 'p'
+  content: {
+    type: "string",
+    source: "html",
+    selector: "p"
   }
-}
+};
 
-registerBlockType('gutenbergs-hotel-blocks/features-block', {
-    title: __('Features Block', 'gutenbergs-hotel-blocks'),
-    description: __('Features', 'gutenbergs-hotel-blocks'),
-    category: 'gh-blocks',
-    attributes,
-    edit,
-    save: ( {attributes} ) => {
-      const {title, content} = attributes;
+registerBlockType("gutenbergs-hotel-blocks/feature", {
+  title: __("Feature", "gutenbergs-hotel-blocks"),
+  description: __("Feature", "gutenbergs-hotel-blocks"),
+  category: "gh-blocks",
+  parent: ["gutenbergs-hotel-blocks/features"],
+  attributes,
+  edit,
+  save: ({ attributes }) => {
+    const { title, content } = attributes;
 
-      return(
-        <div>
-          {title &&
-          <RichText.Content 
-            className = {'wp-block-gutenbergs-hotel-blocks-feature-block__title'}
-            tagName = 'h3'
-            value = {title}
+    return (
+      <div>
+        {title && (
+          <RichText.Content
+            className={"wp-block-gutenbergs-hotel-blocks-feature__title"}
+            tagName="h3"
+            value={title}
           />
-          }
+        )}
 
-          {content &&
-          <RichText.Content 
-            className = {'wp-block-gutenbergs-hotel-blocks-feature-block__content'}
-            tagName = 'p'
-            value = {content}
+        {content && (
+          <RichText.Content
+            className={
+              "wp-block-gutenbergs-hotel-blocks-feature__content"
+            }
+            tagName="p"
+            value={content}
           />
-          }
-        </div>
-      )
-    }
-
+        )}
+      </div>
+    );
+  }
 });
