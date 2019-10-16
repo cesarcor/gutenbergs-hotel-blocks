d42f6bef47ea63db753da80b7d9c828f30092019
Cesar Correchel <cesarcorrechel@gmail.com>
2019-10-15 23:24:05 -0600

    add inspector controls

--- src/blocks/booking-form-block/edit.js
+++ src/blocks/booking-form-block/edit.js
@@ -1,2 +1,44 @@
 import { Component } from "@wordpress/element";
+import { InspectorControls } from "@wordpress/editor";
+import { Panel, PanelBody, PanelRow } from "@wordpress/components";
 import { __ } from "@wordpress/i18n";
+
+class BookingFormEdit extends Component {
+  render() {
+    return (
+      <>
+        <InspectorControls>
+          <PanelBody
+            title={__("Form Styles", "gutenbergs-hotel-blocks")}
+            initialOpen={false}
+          >
+            <PanelRow>{__("Form Type", "gutenbergs-hotel-blocks")}</PanelRow>
+          </PanelBody>
+
+          <PanelBody
+            title={__("Fields", "gutenbergs-hotel-blocks")}
+            initialOpen={false}
+          >
+            <PanelRow>
+              {__("Field Type", "gutenbergs-hotel-blocks")}
+              {__("Label Color", "gutenbergs-hotel-blocks")}
+              {__("Field Color", "gutenbergs-hotel-blocks")}
+            </PanelRow>
+          </PanelBody>
+
+          <PanelBody
+            title={__("Submit Button", "gutenbergs-hotel-blocks")}
+            initialOpen={false}
+          >
+            <PanelRow>{__("Button Color", "gutenbergs-hotel-blocks")}</PanelRow>
+            <PanelRow>
+              {__("Button Radius", "gutenbergs-hotel-blocks")}
+            </PanelRow>
+          </PanelBody>
+        </InspectorControls>
+      </>
+    );
+  }
+}
+
+export default BookingFormEdit;
